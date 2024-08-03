46b6521b96e74f2c7229e791e4db999d1482574b
jpabbasi <joao.abbasi@gmail.com>
2024-08-03 20:46:16 -0300

    🚀 feat: core features of the header and button functionality

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,36 +1,54 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import './header.css';
 import {buttons, contact} from './data';
 import HoverButton from './../hoverButton/hoverButton';
 
 function Header() {
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector('.headerMain');
+      const scrollThreshold = window.innerHeight * 0.7; // Adjust this value to change when the color should change (90% of viewport height)
+
+      if (window.scrollY > scrollThreshold) {
+        header.classList.add('dark');
+      } else {
+        header.classList.remove('dark');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <header className="headerMain">
       <div className="headerContent">
         <div className="headerLogo">
           <h1>
-          Pedro.
+            Pedro.
           </h1>
         </div>
-        <nav className="navLinks">
-          {
-            buttons.map((item) => (
-              <HoverButton
-              key={item.id}
-              title={item.title}
-              link={item.link}
-              icon={item.icon}
-            />
-          ))
-          }
-          <HoverButton
-            title={contact.title}
-            link={contact.link}// Provide the link if any
-            icon={contact.icon}
-          />
-        </nav>
-      </div>
+            <nav className="navLinks">
+                {
+                  buttons.map((item) => (
+                    <HoverButton
+                    key={item.id}
+                    title={item.title}
+                    link={item.link}
+                    icon={item.icon}
+                    variant="default"
+                    />
+                  ))
+                }
+                <HoverButton
+                  title={contact.title}
+                  link={contact.link}
+                  icon={contact.icon}
+                  variant="ghost"
+                  />
+              </nav>
+        </div>
     </header>
   );
 }
