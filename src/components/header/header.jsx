9b811343bb36e65cd1ec1b7d1286188160fa1871
jpabbasi <joao.abbasi@gmail.com>
2024-08-06 17:17:06 -0300

    🚀 header mobile

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import './header.css';
-import {buttons, contact} from './data';
+import { buttons, contact } from './data';
 import HoverButton from './../hoverButton/hoverButton';
+import { TbMenu2 } from "react-icons/tb";
 
 function Header() {
-
   useEffect(() => {
     const handleScroll = () => {
       const header = document.querySelector('.headerMain');
-      const scrollThreshold = window.innerHeight * 0.7; // Adjust this value to change when the color should change (90% of viewport height)
+      const scrollThreshold = window.innerHeight * 0.7;
 
       if (window.scrollY > scrollThreshold) {
         header.classList.add('dark');
@@ -25,30 +25,43 @@ function Header() {
     <header className="headerMain">
       <div className="headerContent">
         <div className="headerLogo">
-          <h1>
-            Pedro.
-          </h1>
+          <h1>Pedro.</h1>
         </div>
-            <nav className="navLinks">
-                {
-                  buttons.map((item) => (
-                    <HoverButton
-                    key={item.id}
-                    title={item.title}
-                    link={item.link}
-                    icon={item.icon}
-                    variant="default"
-                    />
-                  ))
-                }
-                <HoverButton
-                  title={contact.title}
-                  link={contact.link}
-                  icon={contact.icon}
-                  variant="ghost"
-                  />
-              </nav>
+        <nav className="navLinks">
+          {buttons.map((item) => (
+            <HoverButton
+              key={item.id}
+              title={item.title}
+              link={item.link}
+              icon={item.icon}
+              variant="default"
+            />
+          ))}
+          <HoverButton
+            title={contact.title}
+            link={contact.link}
+            icon={contact.icon}
+            variant="ghost"
+          />
+        </nav>
+        <div className="mobileMenuContainer">
+          <div className="mobileContactButton">
+            <HoverButton
+              title={contact.title}
+              link={contact.link}
+              icon={contact.icon}
+              variant="ghost"
+            />
+          </div>
+          <div className="mobileMenuButton">
+            <HoverButton
+              link="#"
+              icon={<TbMenu2 />}
+              variant="ghost"
+            />
+          </div>
         </div>
+      </div>
     </header>
   );
 }
