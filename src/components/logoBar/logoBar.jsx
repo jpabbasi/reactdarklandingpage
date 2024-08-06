bd356a8dd7fd862f9703d166dca46a3bf566c767
jpabbasi <joao.abbasi@gmail.com>
2024-08-06 17:19:00 -0300

    🔧 logoBar refined and added mobile version

--- src/components/logoBar/logoBar.jsx
+++ src/components/logoBar/logoBar.jsx
@@ -1,20 +1,28 @@
+import React from 'react';
 import './logoBar.css';
-import {logos} from './data'
+import { logos } from './data';
 
 const LogoBar = () => {
-    return (
-      <div className="logoBar">
-        <div className="fadeLeft"></div>
-        <div className='logoWrapper'>
+  return (
+    <div className="logoBar">
+      <div className="fadeLeft"></div>
+      <div className='logoWrapper'>
         <div className="logos">
+          {/* duplicated to make the loop infinite */}
           {[...logos, ...logos].map((logo, index) => (
-              <img key={index} src={logo.src} alt={logo.alt} />
-            ))}
+            <div key={index} className="logoContainer">
+              <img
+                src={logo.src}
+                alt={logo.alt}
+                className="logoImage"
+              />
+            </div>
+          ))}
         </div>
-        </div>
-        <div className="fadeRight"></div>
       </div>
-    );
-  };
-  
-  export default LogoBar;
+      <div className="fadeRight"></div>
+    </div>
+  );
+};
+
+export default React.memo(LogoBar);
