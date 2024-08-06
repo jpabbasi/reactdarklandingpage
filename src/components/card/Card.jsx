0a9d72e1f8337ae8d09052c7a6a4d4fcee0e048b
jpabbasi <joao.abbasi@gmail.com>
2024-08-06 17:08:46 -0300

    🚀 added new card variant

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,9 @@
 import './card.css'
 
-const Card = ({children, className, onClick}) => {
+const Card = ({children, className, onClick, variant}) => {
   return (
     <div>
-      <article className={`card ${className}`} onClick={onClick}>
+      <article className={`card ${className} ${variant}`} onClick={onClick}>
         {children}
       </article>
     </div>
