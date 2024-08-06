0a27fa526cda0a36ec4c260ddc57004afe29421e
jpabbasi <joao.abbasi@gmail.com>
2024-08-06 17:21:04 -0300

    🚀 added pastColaborations to home page main component

--- src/sections/home/home.jsx
+++ src/sections/home/home.jsx
@@ -2,6 +2,7 @@ import './home.css'
 import LogoBar from './../../components/logoBar/logoBar';
 import Title from './title/title';
 import ServicesCards from './servicesCards/servicesCards';
+import PastColaborations from './pastColaborations/pastColaborations'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <div className='whiteBoard'>
         {<LogoBar/>}
         {<ServicesCards/>}
+        {<PastColaborations/>}
       </div>
     </div>
   )
