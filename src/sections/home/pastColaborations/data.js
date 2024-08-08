15005523053933f37a97927fe76b71d5d3f65acd
jpabbasi <joao.abbasi@gmail.com>
2024-08-08 18:28:01 -0300

    🔧 changed mockup

--- src/sections/home/pastColaborations/data.js
+++ src/sections/home/pastColaborations/data.js
@@ -2,21 +2,21 @@ import backgroundImage1 from '../../../assets/backgroundImage1.png'
 
 export const cardsData = [
     {
-        title: "ZENVIA'S CONVERSION",
+        title: "CLIENT",
         image: backgroundImage1,
         desc: "This is a description of the card",
         buttonLink: "/learn-more",
         stacks: ["react", "figma"]
     },
     {
-        title: "ZENVIA'S CONVERSION",
+        title: "CLIENT",
         image: backgroundImage1,
         desc: "This is a description of the card",
         buttonLink: "/learn-more",
         stacks: ["react", "figma"]
     },
     {
-        title: "ZENVIA'S CONVERSION",
+        title: "CLIENT",
         image: backgroundImage1,
         desc: "This is a description of the card",
         buttonLink: "/learn-more",
