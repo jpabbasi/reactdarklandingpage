8b3b4b903a07793810866959c48dbaca889e02d3
jpabbasi <joao.abbasi@gmail.com>
2024-08-09 23:44:28 -0300

    🚀 refined reviews css and its data

--- src/sections/home/reviews/data.js
+++ src/sections/home/reviews/data.js
@@ -1,33 +1,69 @@
-import Avatar1 from '../../../assets/avatar1.png'
-import Avatar2 from '../../../assets/avatar2.jpeg'
-import Avatar3 from '../../../assets/avatar3.jpeg'
+import mzr from '../../../assets/mzr.png'
+import mrn from '../../../assets/mrn.jpeg'
+import rznck from '../../../assets/rznck.jpeg'
+import b from '../../../assets/b.jpg'
+import qn from '../../../assets/qn.png'
+import tylr from '../../../assets/tylr.jpeg'
 
 export const reviewsData = [
+  
     {
-      title: "High-quality results with a proactive approach",
-      description: `Pedro is an excellent software engineer with a great sense of teamwork, partnership, and proactivity.
-                    In his day-to-day work, he delivers high-quality results and is concerned with the broader vision of the product he is working on`,
-      avatar: Avatar1,
       name: "Rafael Mazaro",
+      title: "High-end quality results",
+      description: `Pedro is an excellent software engineer with a great sense of teamwork, partnership, and proactivity.
+                    In his day-to-day work, he delivers high-end quality results and is concerned with the broader vision of the product he is working on`,
+      avatar: mzr,
       role: "Director of Technology and Strategy",
     },
     {
+      name: "Abe Jr.",
+      title: "Strongly recommend!",
+      description: `Pedro is one of the most valuable people I have ever worked with.
+                    He understands complicated issues even when outside of his direct area of expertise.
+                    He is very thorough in everything he does and can be depended upon to get the job done.
+                    I would recommend him with any project that requires the very best in it execution.
+                    I have constantly felt Pedro was one of the very best team mates
+                    I have ever had the opportunity of working with. Strongly recommend!`,
+      avatar: b,
+      role: "Head of Product Design",
+    },
+    {
+      name: "Roberto Reznicek",
       title: "A valuable asset to any organization",
       description: `I'm pleased to recommend Pedro for any future opportunity.
                     He demonstrated exceptional interpersonal skills and an impressive ability to work effectively within a team.
                     Pedro quickly mastered new skills and technologies, showcasing dedication, a positive attitude, and eagerness to learn.
                     He's a valuable asset to any organization and will undoubtedly excel in any role he undertakes.`,
-      avatar: Avatar2,
-      name: "Roberto Reznicek",
+      avatar: rznck,
       role: "Software Development Manager",
-    },{
-      title: "Impresses with his sheer determination and eagerness to learn",
+    },
+    {
+      name: "Edgardo Moreno",
+      title: "A fantastic addition to any team",
       description: `Pedro is the type of team member who actively seeks out answers and tackles new technologies and concepts with determination.
                     He learns quickly, supports others, and approaches his work with passion and a hardworking attitude.
                     Pedro is a fantastic addition to any team, always bringing positive energy`,
-      avatar: Avatar3,
-      name: "Edgardo Moreno",
+      avatar: mrn,
       role: "Fullstack Engineer",
-    }
+    },
+    {
+      name: "Leonardo Aquino",
+      title: "Communication and proactivity are strong points",
+      description: `Excellent professional with outstanding qualities.
+                    Communication and proactivity are strong points.
+                    He is always eager to learn new things and has great reasoning
+                    skills that help in discussions about important decisions during
+                    the development process of a tool.`,
+      avatar: qn,
+      role: "Senior Software Engineer",
+    },
+    {
+      name: "Taylor Byk Schinvelski",
+      title: "Consistently proactive",
+      description: `A great professional to have on the team, always
+                    willing to learn and take on new tasks. He is consistently proactive,
+                    and his speaking and communication skills are among his strengths.`,
+      avatar: tylr,
+      role: "Senior Software Engineer",
+    },
   ];
-  
