6915980d603ccbeda0465ab9b9adcf7231f1de72
jpabbasi <joao.abbasi@gmail.com>
2024-08-10 15:38:25 -0300

    🚀 added more data to test the component properly

--- src/sections/home/works/data.js
+++ src/sections/home/works/data.js
@@ -1,4 +1,6 @@
 import backgroundImage1 from '../../../assets/backgroundImage1.jpg'
+import backgroundImage2 from '../../../assets/backgroundImage2.jpg'
+import backgroundImage3 from '../../../assets/backgroundImage3.jpg'
 
 export const cardsData = [
     {
@@ -10,9 +12,16 @@ export const cardsData = [
     },
     {
         title: "CLIENT",
-        image: backgroundImage1,
+        image: backgroundImage2,
         desc: "This is a description of the card",
         buttonLink: "/learn-more",
-        stacks: ["react", "figma"]
+        stacks: ["react"]
+    },
+    {
+        title: "CLIENT",
+        image: backgroundImage3,
+        desc: "This is a description of the card",
+        buttonLink: "/learn-more",
+        stacks: ["react"]
     }
 ]
