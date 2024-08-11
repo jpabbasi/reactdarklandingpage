2cd83f1265279ad8b5a50c5ad48e07bbf00f1773
jpabbasi <joao.abbasi@gmail.com>
2024-08-10 23:14:23 -0300

    🚀 added arrow-up variant and minor changes on other arrows

--- src/components/hoverButton/hoverButton.jsx
+++ src/components/hoverButton/hoverButton.jsx
@@ -1,20 +1,32 @@
 import { Link } from 'react-router-dom';
 import { FiArrowUpRight } from 'react-icons/fi';
+import { FiArrowUp } from "react-icons/fi";
 import './hoverButton.css';
 
 function HoverButton({ title, link, icon, variant }) {
+  const renderArrowIcon = () => {
+    if (variant === 'arrow-up') {
+      return <FiArrowUp className="icon" />;
+    } else if (variant.includes('arrow')) {
+      return <FiArrowUpRight className="icon" />;
+    }
+    return null;
+  };
+
   return (
     <Link to={link} className={`hoverButton ${variant}`}>
       <div className="hoverButtonContent">
-      {(variant === 'arrow' || variant === 'arrow-white') && <FiArrowUpRight className="icon" />}
+        {renderArrowIcon()}
         {icon}
-        { title && (
-        <div className="hoverButtonTitle">
+        {variant.includes('arrow') ? (
+          <span>{title}</span>
+        ) : (
+          <div className="hoverButtonTitle">
           <span className="textUp">{title}</span>
           <span className="textDown">{title}</span>
         </div>
-    )
-  }
+        )
+        }
   </div>
     </Link>
   );
