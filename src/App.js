1887951dd9c19ec809e91bd30bc7ddc446d90ea7
jpabbasi <joao.abbasi@gmail.com>
2024-08-12 11:14:34 -0300

    🚀 added page 404

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './sections/home/home'
-import Header from './components/header/header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './sections/home/home';
+import Header from './components/header/header';
 import Footer from './components/footer/footer';
+import NotFound from './sections/notFound/notFound';
+import Services from './sections/services/services';
 
 function App() {
-  
+  const Layout = ({ children }) => (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+
   return (
     <Router>
-      <Header/>
-      <main>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/services" element={<Layout><Services /></Layout>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      </main>
-      <Footer />
     </Router>
   );
 }
---
+++ src/sections/notFound/notFound.jsx
@@ -0,0 +1,18 @@
+import './notFound.css'
+import HoverButton from './../../components/hoverButton/hoverButton';
+
+function NotFound() {
+  return (
+    <div className="pageNotFound">
+      <h1>404</h1>
+      <div>
+        <h2>Oops! Page not found</h2>
+        <p>We couldn’t find the page you were looking for. It might have been removed, had its name changed, or is temporarily unavailable.</p>
+      </div>
+      <HoverButton link="/" variant="white" title="BACK TO HOMEPAGE"/>
+    </div>
+  );
+}
+
+export default NotFound;
+
