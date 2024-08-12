c0e02676ce2fe695cdf3fa3ccf0765b2c7805921
jpabbasi <joao.abbasi@gmail.com>
2024-08-12 13:00:49 -0300

    🔧 added link tag

--- src/components/header/data.js
+++ src/components/header/data.js
@@ -6,20 +6,20 @@ import { TbMessage2 } from "react-icons/tb";
 export const buttons = [
     {id: 1,
     title: 'SERVICES',
-    link: '/',
+    link: '/services',
     icon: <TbBriefcase2 />},
     {id: 2,
     title: 'WORK',
-    link: '/',
+    link: '/work',
     icon: <TbDeviceImac />},
     {id: 3,
     title: 'ABOUT',
-    link: '/',
+    link: '/about',
     icon: <TbLego />},
 ]
 
 export const contact = {
     title: "LET'S TALK",
     icon: <TbMessage2 />,
-    link: '/',
+    link: '/letstalk',
 }
