6085497723f8e20261c6cc29f7842c7f3e02bd94
jpabbasi <joao.abbasi@gmail.com>
2024-08-13 16:56:44 -0300

    🚀 data for milestones

---
+++ src/sections/work/data.js
@@ -0,0 +1,19 @@
+import { TbRocket } from "react-icons/tb";
+import { TbTool } from "react-icons/tb";
+import { TbTrophy } from "react-icons/tb";
+
+
+export const milestones = [
+    {
+        icon: <TbRocket/>,
+        title: `Successfully led the complete development cycle of multiple projects, transforming initial concepts into fully functional, high-performance applications.`
+    },
+    {
+        icon: <TbTool/>,
+        title: `Reinvigorated and optimized numerous legacy applications, significantly improving functionality and user experience, resulting in increased user satisfaction and retention.` 
+    },
+    {
+        icon: <TbTrophy/>,
+        title: `Recognized as Frontend Specialist of the Year for exceptional expertise in resolving critical bugs and enhancing the user experience during a high-stakes rebranding project.`
+    }
+]
