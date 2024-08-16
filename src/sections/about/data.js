5785c77ee83ebc11b2da055d630fabb1b2c8e902
jpabbasi <joao.abbasi@gmail.com>
2024-08-16 15:58:07 -0300

    🚀 data for about WIP

---
+++ src/sections/about/data.js
@@ -0,0 +1,52 @@
+import { LuGraduationCap } from "react-icons/lu";
+import { TbAward } from "react-icons/tb";
+
+export const process =
+    [
+        {   
+        id: '01',
+        title: 'Building Trust',
+        desc: `My approach is built on trust, transparency, and a deep commitment to understanding your unique needs. Together, we'll create something truly amazing.`
+        },
+        {   
+        id: '02',
+        title: 'Building Partnerships',
+        desc: `Collaboration, communication, and a deep understanding
+        of your brand and audience are the keys to a successful partnership.
+        Let's work together to achieve your goals.`
+        },
+        {   
+        id: '03',
+        title: 'Achieving Real Results',
+        desc: `With a focus on clear communication, attention to detail,
+        and a passion for excellence, I'm here to help you achieve your goals and
+        take your business to the next level.`
+        }
+]
+
+export const cardsData = [
+        {
+            icon: <LuGraduationCap/>,
+            title: 'Awwwards SOTD',
+            date: '2022',
+            source: 'SENAI SC'
+        },
+        {
+            icon: <TbAward/>,
+            title: 'Awwwards SOTD',
+            date: '2022',
+            source: 'SENAI SC'
+        },
+        {
+            icon: <TbAward/>,
+            title: 'Awwwards SOTD',
+            date: '2022',
+            source: 'SENAI SC'
+        },
+        {
+            icon: <TbAward/>,
+            title: 'Awwwards SOTD',
+            date: '2022',
+            source: 'SENAI SC'
+        },
+]
